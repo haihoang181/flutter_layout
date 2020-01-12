711d5c77e62d1fea02b43e447144a1cfd813ab16
haihoang181 <haihoang181@gmail.com>
2020-01-12 22:57:26 +0700

    layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,46 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = new Container(
+      padding: const EdgeInsets.all(10.0),
+      child: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: new Text("Programming tutorial Channel",
+                    style: new TextStyle(fontSize: 22.0,fontWeight: FontWeight.bold),),
+                ),
+                
+                new Text("This Channel contains tutorial videos in Flutter, React-native, React, Angular",
+                    style: new TextStyle(fontSize: 22.0,color:Colors.grey[600]),),
+              ],
+            ),
+          ),
+           
+           new Icon(Icons.favorite,color: Colors.red),
+           new Text(" 100",style: new TextStyle(fontSize: 22.0,color: Colors.grey[600]),),
+        ],
+      ),
+    );
     return new MaterialApp(
       title: "flutter_layout",
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text("My App"),
         ),
-        body: new Center(
-          child: new Text("Body"),
+        body:  new ListView(
+          children: <Widget>[
+            new Image.asset(
+              'images/eye.jpg',
+              fit: BoxFit.cover,
+            ),
+
+           titleSection
+          ],
         ),
       ),
 
